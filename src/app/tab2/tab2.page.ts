b002485517ce6cbfb4bdd6d3e58ceedd64130e8b
Ana Sequeira <anacseq@gmail.com>
2020-07-28 20:09:13 +0100

    working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 import { Dialogs } from '@ionic-native/dialogs/ngx';
 import { Platform } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -10,14 +12,28 @@ import { Platform } from '@ionic/angular';
 export class Tab2Page {
   qrScan: any;
 
+
   constructor(
     private qrScanner: QRScanner,
     private dialogs: Dialogs,
-    private platform: Platform) {
+    private platform: Platform,
+    public alertController: AlertController) {
     this.platform.backButton.subscribeWithPriority(0, () => {
       document.getElementsByTagName("body")[0].style.opacity = "1";
       this.qrScan.unsubscribe();
     })
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
   scanQrCode() {
